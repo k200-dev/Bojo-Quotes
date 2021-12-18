785a2fd736914cd6910eaa4157c50157bce9d609
k200-dev <85361508+k200-dev@users.noreply.github.com>
2021-12-18 19:38:51 +0000

    CORS Headers
    Working CORS headers for web requests

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -3,13 +3,28 @@
 #[macro_use] extern crate rocket;
 #[macro_use] extern crate serde_derive;
 
+use rocket_cors::{AllowedHeaders};
+use rocket::http::Method;
+
 mod endpoints {
     pub mod random;
 }
 pub mod get_quote;
 
 fn main() {
-    rocket::ignite().mount("/random", routes![endpoints::random::random]).launch();
+    let cors = rocket_cors::CorsOptions {
+        allowed_methods: vec![Method::Get].into_iter().map(From::from).collect(),
+        allowed_headers: AllowedHeaders::some(&["Authorization", "Accept"]),
+        allow_credentials: true,
+        ..Default::default()
+      }
+      .to_cors()
+      .expect("Error creating cors");
+
+    rocket::ignite()
+    .mount("/random", routes![endpoints::random::random])
+    .attach(cors)
+    .launch();
 }
 
 
