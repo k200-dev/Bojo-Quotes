5ea68f2a56b8e4b9012230f668be19d9fe1e96c7
k200-dev <85361508+k200-dev@users.noreply.github.com>
2021-12-19 13:19:15 +0000

    Fixed naming issues left over from refactoring

--- backend/src/endpoints/random.rs
+++ backend/src/endpoints/random.rs
@@ -8,23 +8,23 @@ use rand::seq::IteratorRandom;
 #[derive(Serialize)]
 pub struct Quote {
     pub quote: String,
-    pub source: String,
+    pub subject: String,
 }
 
 #[get("/")]
 pub fn random() -> Json<Quote> {
-    let f = File::open("quotes.txt")
+    let f = File::open("../../src/quotes.txt")
     .expect("Could not read file");
     let f  = BufReader::new(f);
     let lines = f.lines().map(|l| l.expect("Couldn't read line"));
-    let quote = lines.choose(&mut rand::thread_rng()).expect("Error");
+    let quote = lines.choose(&mut rand::thread_rng()).expect("Error choosing a line");
     let split_quote = quote.split("|");
     let quote_vec: Vec<&str> = split_quote.collect();
     Json(Quote {
         quote: quote_vec[0]
             .trim()    
             .to_string(),
-        source: quote_vec[1]
+        subject: quote_vec[1]
             .trim()
             .to_string(),
     })
